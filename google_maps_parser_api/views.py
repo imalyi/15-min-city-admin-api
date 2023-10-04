73276e18f6686ccc73af63a641dee51ef40ae216
imalyi <malyyigor34@gmail.com>
2023-10-04 14:29:15 +0200

    add views for primitive models

--- google_maps_parser_api/views.py
+++ google_maps_parser_api/views.py
@@ -1,9 +1,27 @@
 from rest_framework.generics import ListCreateAPIView
 
-from google_maps_parser_api.serializers import ClientSerializer
-from google_maps_parser_api.models import Client
+from google_maps_parser_api.serializers import ClientSerializer, CredentialSerializer, PlaceTypeSerializer, StatusSerializer, CoordinateSerializer
+from google_maps_parser_api.models import Client, Credential, PlaceType, Status, Coordinate
 
 
 class ClientView(ListCreateAPIView):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
+
+
+class CredentialView(ListCreateAPIView):
+    serializer_class = CredentialSerializer
+    queryset = Credential.objects.all()
+
+class PlaceTypeView(ListCreateAPIView):
+    serializer_class = PlaceTypeSerializer
+    queryset = PlaceType.objects.all()
+
+
+class StatusView(ListCreateAPIView):
+    serializer_class = StatusSerializer
+    queryset = Status.objects.all()
+
+class CoordinatesView(ListCreateAPIView):
+    serializer_class = CoordinateSerializer
+    queryset = Coordinate.objects.all()
