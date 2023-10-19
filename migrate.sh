3e4442207e93db9a17f9830007f80928a8fea4d9
igor <malyyigor34@gmail.com>
2023-10-19 19:12:29 +0200

    change mirgrations

--- migrate.sh
+++ migrate.sh
@@ -4,9 +4,6 @@ rm db.sqlite3
 rm -r google_maps_parser_api/migrations/
 #sudo docker-compose up -d
 
-python3 manage.py makemigrations
-python3 manage.py migrate
-
 python3 manage.py makemigrations users
 python3 manage.py migrate users
 
@@ -15,6 +12,9 @@ python3 manage.py migrate google_maps_parser_api
 
 python3 manage.py makemigrations gmaps
 python3 manage.py migrate gmaps
+
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 DJANGO_SUPERUSER_USERNAME=igor \
 DJANGO_SUPERUSER_PASSWORD=343877 \
