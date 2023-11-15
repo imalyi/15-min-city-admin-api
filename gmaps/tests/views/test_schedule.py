ab25b4af97e421ad97bc8d19092fd126742e8f6d
igor <malyyigor34@gmail.com>
2023-11-15 21:17:39 +0100

    add test for PUT /schedule

--- gmaps/tests/views/test_schedule.py
+++ gmaps/tests/views/test_schedule.py
@@ -36,3 +36,19 @@ class TestSchedule(APITestCase):
         response = self.client.get(url)
         self.assertEquals(response.status_code, HTTP_401_UNAUTHORIZED)
 
+    def test_update_task_template_authorised(self):
+        url = reverse('schedule-detail', str(self.schedule.id))
+        data = {
+            "name": "new_name",
+            "day_of_month": "*",
+            "minute": "*",
+            "hour": "*"
+        }
+        response = self.client.put(url, data=data, HTTP_AUTHORIZATION=self.access_token)
+        self.assertEquals(response.status_code, HTTP_200_OK)
+
+        response = self.client.get(reverse('schedule-detail', str(self.schedule.id)),
+                                   HTTP_AUTHORIZATION=self.access_token)
+        self.assertEquals(response.status_code, HTTP_200_OK)
+        serialized_data = ScheduleSerializer(response.data).data
+        self.assertEquals(serialized_data, response.data)
